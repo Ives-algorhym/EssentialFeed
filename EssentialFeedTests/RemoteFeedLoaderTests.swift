8130f358865ad8873fb86059962c460053296ea2
ives-algorhym <algoives@icloud.com>
2023-12-09 21:41:54 -0800

    Guarantee we only receive one error

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -34,6 +34,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+
+        client.error = NSError(domain: "Test", code: 0)
+
+        var capturedErrors = [RemoteFeedLoader.Error]()
+
+        sut.load { capturedErrors.append($0) }
+
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+
+    // MARK: Helpers
+
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -42,8 +56,13 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+
+            if let error  = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }    
